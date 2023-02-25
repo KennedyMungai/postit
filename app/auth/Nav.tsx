4dcdbefac89d22c580d2596138efd29b3d323012
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-02-25 12:41:50 +0300

    Added styles for the h1 tag

--- app/auth/Nav.tsx
+++ app/auth/Nav.tsx
@@ -7,7 +7,7 @@ const Nav = () =>
     return (
         <nav className='flex justify-between items-center py-8'>
             <Link href={'/'}>
-                <h1>Send it.</h1>
+                <h1 className='font-bold text-lg'>Send it.</h1>
             </Link>
             <ul>
                 <Login />

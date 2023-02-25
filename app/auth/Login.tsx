59fe84f0dc85ff7b87151f1a8363a04d13261067
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-02-25 12:45:05 +0300

    Added styles for the button

--- app/auth/Login.tsx
+++ app/auth/Login.tsx
@@ -7,7 +7,7 @@ const Login = () =>
     return (
         <li className='list-none'>
             <button
-                className="text-sm"
+                className="text-sm bg-gray-700 text-white py-2 px-6 rounded-xl disabled:opacity-25"
                 onClick={() => signIn()}
             >
                 Sign In

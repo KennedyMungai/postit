7498da5880157b933053f5ab469d2ade9fb774c6
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-02-25 12:35:46 +0300

    Added a button to the Login component

--- app/auth/Login.tsx
+++ app/auth/Login.tsx
@@ -4,7 +4,13 @@ import { signIn } from 'next-auth/react'
 
 const Login = () =>
 {
-
+    return (
+        <li className='list-none'>
+            <button className="text-sm">
+                Sign In
+            </button>
+        </li>
+    )
 }
 
 export default Login

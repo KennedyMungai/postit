7bd1c34ce0e642aded2f1862d1fd2b7de85367af
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-02-27 10:00:45 +0300

    Called the prisma adapter outside the component function

--- app/auth/[...nextauth].js
+++ app/auth/[...nextauth].js
@@ -1,7 +1,10 @@
+import { PrismaAdapter } from "@next-auth/prisma-adapter"
 import NextAuth from "next-auth"
 import GithubProvider from "next-auth/providers/github"
 import GoogleProvider from "next-auth/providers/google"
 import prisma from '../../prisma/client'
+
+const adapter = PrismaAdapter(prisma)
 
 export const authOptions = {
     // Configure one or more authentication providers
